93d61b58dafa236d1add4442676a402e456f784d
ErMAK <ermakov_kirill@mail.ru>
2023-05-09 16:59:27 +0200

    данные для анализа готовы

--- alarm_bot.py
+++ alarm_bot.py
@@ -3,18 +3,19 @@ import telegram
 import time
 import os
 import asyncio
-from ip_rotate import proxys
+# from ip_rotate import proxys
 
 # Создаем объект бота
 bot = telegram.Bot(token='5674190609:AAFfqNuRVyJJzU0_aOGiDy6e7S8Q5iPGtus')
 
 # Отправляем сообщение
 async def send_message():
-    await bot.send_message(chat_id='257661905', text='Ваш код завершил работу! \n'+'cnt_200 ='+ str(proxys[1]) + '\n cnt_400 =' + str(proxys[2]))
+    await bot.send_message(chat_id='257661905', text='Ваш код завершил работу!')
 # Запускаем ваш код
 async def run_code():
     # здесь ваш код
-    os.system('ip_rotate.py')
+    os.system('parser.py')
+    # os.system('ip_rotate.py')
     time.sleep(1) # пример задержки в 10 секунд
 
 # Запускаем код и отправляем сообщение
